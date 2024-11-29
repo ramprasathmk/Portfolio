121d3dafb6092ee6fd9f829367700081e0fe0d8a
ramprasathmk <mkramprasath12@gmail.com>
2024-11-29 21:25:51 +0545

    chore: About component modified
    Signed-off-by: ramprasathmk <mkramprasath12@gmail.com>

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
+import { Link } from 'react-router-dom';
 
 function AboutCard() {
   return (
@@ -8,13 +9,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple"> Ramprasath M K </span>
+            from <span className="purple"> Tamil Nadu, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I'm currently pursuing my Bachelor's in Information Technology at <Link className='purple' to="https://aamec.edu.in">AAMEC</Link>.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,17 +22,14 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "A Myth is a Myth until you experiencing it!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Ramprasath M K</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,25 +3,37 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
-  SiSlack,
   SiVercel,
-  SiMacos,
+  SiWindows11,
+  SiDocker,
+  SiPycharm,
+  SiEclipseide,
+  SiGithub,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows11 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <SiPycharm />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+        <SiEclipseide />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
